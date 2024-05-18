32a387616bb8006f16260381ccccd9e2bbe44035
Brunella Quaye <131973976+brunellaquaye@users.noreply.github.com>
2024-05-18 14:44:22 +0000

    Task3: Complete Task 3

--- userInfo.js
+++ userInfo.js
@@ -1,16 +1,11 @@
-function createUserProfiles(strings, modifiedName) {
+function createUserProfiles(strings, newFormatArray) {
     let newUserProfile=[];
     for (let i = 0; i < strings.length; i++){
         newUserProfile.push({
             id:i+1,
             originalName:strings,
-            modifiedName:modifiedName[i],
+            newFormatArray:newFormatArray[i],
         });
         return newUserProfile;
     }
 }
-
-let oldName = ["Kojo","Ben","John"];
-let newName = ["KOJO","GEN","JOHN"];
-let egProfile = createUserProfiles(oldName, newName);
-console.log(egProfile);
