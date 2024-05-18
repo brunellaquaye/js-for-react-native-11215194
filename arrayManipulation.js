7654a4f6ad416d314c9e8723f1e2381f8091d403
Brunella Quaye <131973976+brunellaquaye@users.noreply.github.com>
2024-05-18 14:20:26 +0000

    Task 1: Changes made to square even numbers instead of doubling them.

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,8 +1,9 @@
+// Task 1
 function processArray(array) {
   let newProcessArray = [2,3,3,5,7,5,8];
   for (let i = 0; i < newProcessArray.length; i++){
     if (newProcessArray[i] % 2 === 0){
-        newProcessArray[i] = newProcessArray[i]*2;
+        newProcessArray[i] = newProcessArray[i]*newProcessArray[i];
     }else {
         newProcessArray[i] = newProcessArray[i]*3;
     }
